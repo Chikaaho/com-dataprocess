e0b8aad93e2f2428190c6e347eeb403142c2d166
huangjiaxin_yuns <huangjiaxin@yunsucoding.com>
2023-09-05 11:14:06 +0800

    更新框架

--- src/main/java/com/dataprocess/core/controller/DataProcessController.java
+++ src/main/java/com/dataprocess/core/controller/DataProcessController.java
@@ -3,9 +3,13 @@ package com.dataprocess.core.controller;
 import com.dataprocess.api.apiEnums.ApiStatus;
 import com.dataprocess.api.request.Result;
 import com.dataprocess.core.data.process.service.DataProcessService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * @Author: hjx
@@ -14,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @Description: 手动执行调度任务
  */
 @RestController("/dataProcess")
+@Slf4j
 public class DataProcessController {
 
     private DataProcessService dataProcessService;
@@ -22,8 +27,10 @@ public class DataProcessController {
     public Result startProcessMiddleTable() {
         try {
             dataProcessService.queryDetails();
+            log.info("'{}-'手工成功执行一次数据调度", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
             return Result.success(ApiStatus.SUCCESS);
         } catch (Exception e) {
+            log.error("手工执行调度失败,异常信息:{}",e.toString());
             return Result.error(ApiStatus.ERROR, e.toString());
         }
     }
